bd142cbdcb30a6cc22c48d181c53b70c5f3fa73d
Renato Cintra <renatoqcintra@gmail.com>
2026-05-06 10:34:26 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$n1=readline("Digite a first nota: ");
+$n2=readline("Digite a segunda nota: ");
+$n3=readline("Digite a terceira nota: ");
+$n4=readline("Digite a quarta nota: ");
+$med=($n1+$n2+$n3+$n4)/4;
+
+echo "A média das notas é: $med";
+?>
