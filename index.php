3e93be603ddf56963ea48d2dadacbcbebd6e77bf
Renato Queiroz Cintra <renatoqcintra@gmail.com>
2026-05-13 11:36:10 -0300

    commitnovabranch

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $n1=readline("Digite a first nota: ");
 $n2=readline("Digite a segunda nota: ");
 $n3=readline("Digite a terceira nota: ");
-$n4=readline("Digite a quarta nota: ");
+$n4=readline("Digite a 4 nota: ");
 $freq=readline("Digite a frequência: ");
 $med=($n1+$n2+$n3+$n4)/4;
 
